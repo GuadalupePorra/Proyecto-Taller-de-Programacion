d81af4cf25d843fa2da2ca42b329f87676867437
GuadalupePorra <133470650+GuadalupePorra@users.noreply.github.com>
2023-10-25 20:10:05 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+let menuBtn = document.querySelector('#menu');
+let navbar= document.querySelector('.header .f .navbar');
+
+menuBtn.onclick = () =>{
+    menuBtn.classList.toggle('fas-bars');
+    navbar.classList.toggle('active');
+}  
+
+var swiper = new Swiper(".cursosSlider", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    breakpoints:{
+
+    
+        400: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+
+        600: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+
+    }
+  });
